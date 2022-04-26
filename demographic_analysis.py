e08d765e972e947934164e07da30ca7f714938f7
Keshav Sompura <keshavsomp@gmail.com>
2022-04-25 18:56:15 -0700

    first commit

---
+++ demographic_analysis.py
@@ -0,0 +1,23 @@
+# Demographic data analysis from 1994 Census data
+
+#import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# read in the data
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+data = pd.read_csv(url)
+
+data.head()
+data.shape
+data.info
+col_names = ['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race','sex','capital-gain','capital-loss','hours-per-week','native-country','salary']
+
+
+data.columns = col_names
+
+# How many people of each race are represented in this dataset? This should be a Pandas series with race names as the index labels. 
+
+
+

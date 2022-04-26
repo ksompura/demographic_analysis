c5ec3863ed838cb15efa6b94103fa6b023bed263
Keshav Sompura <keshavsomp@gmail.com>
2022-04-25 20:22:58 -0700

    question 2 finish

--- demographic_analysis.py
+++ demographic_analysis.py
@@ -4,10 +4,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # read in the data
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
-data = pd.read_csv(url)
+data = pd.read_csv(url, skipinitialspace=True)
 
 data.head()
 data.shape
@@ -16,15 +17,32 @@ col_names = ['age','workclass','fnlwgt','education','education-num','marital-sta
 
 data.columns = col_names
 
-# How many people of each race are represented in this dataset? This should be a Pandas series with race names as the index labels. 
+# 1. How many people of each race are represented in this dataset? This should be a Pandas series with race names as the index labels. 
 df = data
 
 df_race = df.groupby(['race'])['race'].count()
-df_race.sort_values(ascending=False)
+df_race = df_race.sort_values(ascending=False)
+print(df_race)
 ## We can see that race: 'white' is the large majority group being represented in this dataset, followed by 'Black', and 'Asian-Pac-Islander'.
 
 # plot race representation
+df_race.plot.bar()
+sns.set_style('darkgrid')
+#plt.xlabel('Race')
+#plt.ylabel('Count of Individuals')
+#plt.title('Race Representation')
+#plt.legend()
+plt.show()
 
 
 
+# 2. What is the average age of men?
 
+# make a mask to only select men
+mask = df['sex'] == 'Male'
+df_male = df[mask]
+df_male['age'].mean()
+## average male age is 39.434 years old.
+# consider graphing age distribution
+
+# 3.
